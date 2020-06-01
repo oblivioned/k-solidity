5b67cdfa17b9626d965fac2f52a9900179bf1b30
oblivioned <371397938@qq.com>
2020-06-01 16:00:17 +0800

    import

---
+++ spec/k-solidity-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KSolidityView from '../lib/k-solidity-view';
+
+describe('KSolidityView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
